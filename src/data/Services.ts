1ebf1e9b125101101b015d78b9e2c301dc56a460
hyuga-szk <m.turanui@gmail.com>
2025-10-17 13:01:52 +0900

    change git file path

--- src/data/Services.ts
+++ src/data/Services.ts
@@ -3,36 +3,36 @@ export const services = [
     title: "Speed",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde debitis minus repellat odit modi! Expedita!",
-    url: "/src/assets/gif/rocket-unscreen.gif",
+    url: "/gif/rocket-unscreen.gif",
   },  
   {
     title: "Save Money",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde debitis minus repellat odit modi! Expedita!",
-    url: "/src/assets/gif/save-money-unscreen.gif",
+    url: "/gif/save-money-unscreen.gif",
   },  
   {
     title: "Award",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde debitis minus repellat odit modi! Expedita!",
-    url: "/src/assets/gif/award-unscreen.gif",
+    url: "/gif/award-unscreen.gif",
   },  
   {
     title: "Shopping",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde debitis minus repellat odit modi! Expedita!",
-    url: "/src/assets/gif/shopping-unscreen.gif",
+    url: "/gif/shopping-unscreen.gif",
   },  
   {
     title: "Shield",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde debitis minus repellat odit modi! Expedita!",
-    url: "/src/assets/gif/shield-unscreen.gif",
+    url: "/gif/shield-unscreen.gif",
   },  
   {
     title: "Line Chart",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde debitis minus repellat odit modi! Expedita!",
-    url: "/src/assets/gif/line-chart-unscreen.gif",
+    url: "/gif/line-chart-unscreen.gif",
   },  
 ];
